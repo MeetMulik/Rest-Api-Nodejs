e6230b58a050c2d43c27e92e6614359e6a6648bd
Meet Mulik <89148021+MeetMulik@users.noreply.github.com>
2023-11-30 00:37:28 +0530

    feat: delete posts endpoint

--- controllers/postController.js
+++ controllers/postController.js
@@ -29,4 +29,27 @@ const createPost = async (req, res) => {
   }
 };
 
-export { createPost };
+const deletePost = async (req, res) => {
+  try {
+    const currentUser = req.user;
+    if (!currentUser) return res.status(400).json({ message: "Unauthorized" });
+
+    const { postId } = req.params;
+    console.log("postId", postId);
+
+    const post = await Post.findById({ _id: postId });
+    if (!post) return res.status(404).json({ message: "Post not found" });
+
+    if (post.postedBy.toString() !== currentUser._id.toString())
+      return res.status(401).json({ message: "Unauthorized to delete post" });
+
+    const deletedPost = await Post.findByIdAndDelete({ _id: postId });
+
+    res.status(200).json({ message: "Post deleted successfully" });
+  } catch (error) {
+    console.log("[DELETE POST ERROR]", error.message);
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export { createPost, deletePost };
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import protectRoute from "../middlewares/authMiddleware.js";
 
-import { createPost } from "../controllers/postController.js";
+import { createPost, deletePost } from "../controllers/postController.js";
 
 const router = express.Router();
 
 router.post("/create", protectRoute, createPost);
+router.delete("/:postId", protectRoute, deletePost);
 
 export default router;
