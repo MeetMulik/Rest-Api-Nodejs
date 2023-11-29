27fdda396338ea8107a9f6588d0a1dbc96b05f27
Meet Mulik <89148021+MeetMulik@users.noreply.github.com>
2023-11-29 21:49:34 +0530

    feat: mongodb connection

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import connectDB from "./db/connectDB.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+// Connect to DB
+connectDB();
 
 // Middlewares
 
